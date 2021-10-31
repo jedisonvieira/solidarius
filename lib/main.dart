f7f1a2cccd09729281f5dd17e630a649be4cb7fc
jedison vieira <jedison.vieira@hotmail.com>
2021-10-31 12:11:14 -0300

    projeto iniciado com firebase conectado e funcionando

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  runApp(const MyApp());
+  await Firebase.initializeApp();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text("BORA porra"),
+        ),
+      ),
+    );
+  }
+}
