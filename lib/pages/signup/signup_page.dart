3cd7f62d5100900a87e7f045fc4598f126d74f45
jedison vieira <jedison.vieira@hotmail.com>
2021-12-02 13:06:39 -0300

    singup form

--- lib/pages/signup/signup_page.dart
+++ lib/pages/signup/signup_page.dart
@@ -1,13 +1,22 @@
 import 'package:solidarius/shared/models/user_model.dart';
 import 'package:flutter/material.dart';
 
-class SignupPage extends StatelessWidget {
+class SignupPage extends StatefulWidget {
   final UserModel model;
+
+  const SignupPage(this.model, {Key? key}) : super(key: key);
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final _ageController = TextEditingController();
+  final _nameController = TextEditingController();
+  final _phoneController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
-  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-  SignupPage(this.model, {Key? key}) : super(key: key);
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +39,49 @@ class SignupPage extends StatelessWidget {
                       }
                     }),
                 TextFormField(
+                    controller: _nameController,
+                    keyboardType: TextInputType.name,
+                    decoration:
+                        const InputDecoration(labelText: "Nome completo"),
+                    validator: (email) {
+                      if (email!.isEmpty) {
+                        return "nome inválido";
+                      }
+                    }),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Flexible(
+                        flex: 1,
+                        child: TextFormField(
+                            controller: _ageController,
+                            keyboardType: TextInputType.number,
+                            decoration:
+                                const InputDecoration(labelText: "Idade"),
+                            validator: (email) {
+                              if (email!.isEmpty) {
+                                return "idade inválida";
+                              }
+                            })),
+                    const VerticalDivider(),
+                    Flexible(
+                      flex: 2,
+                      child: TextFormField(
+                          controller: _phoneController,
+                          keyboardType: TextInputType.phone,
+                          decoration:
+                              const InputDecoration(labelText: "Telefone"),
+                          validator: (email) {
+                            if (email!.isEmpty) {
+                              return "telefone inválido";
+                            }
+                          }),
+                    ),
+                  ],
+                ),
+                TextFormField(
                     controller: _passwordController,
+                    keyboardType: TextInputType.visiblePassword,
                     decoration: const InputDecoration(
                       labelText: "Senha",
                     ),
@@ -47,7 +98,11 @@ class SignupPage extends StatelessWidget {
                       child: const Text("Registrar-se"),
                       style: ElevatedButton.styleFrom(primary: Colors.blue),
                       onPressed: () {
-                        if (_formKey.currentState!.validate()) {}
+                        if (_formKey.currentState!.validate()) {
+                          print(_nameController.text);
+                          print(_emailController.text);
+                          print(_passwordController.text);
+                        }
                       },
                     ))
                   ],
