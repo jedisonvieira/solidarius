21211fa7b615cfa0d8d3e84411d7d31c2605473e
jedison vieira <jedison.vieira@hotmail.com>
2021-12-02 08:18:30 -0300

    fixed flutter firebase error

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
